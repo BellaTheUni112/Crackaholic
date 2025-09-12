51e1f47678c5145fc5fde6c0f18f51d21450ec13
Turkey <bellatheunicorn521@gmail.com>
2025-09-12 23:02:14 +0100

    Add files via upload

---
+++ Crackaholic - Made by Turkey/Crackaholic.bat
@@ -0,0 +1,38 @@
+@echo off
+echo Binary must be root!
+set /p x="Directory: "
+if not exist "%x%" (
+	echo Directory doesn't exist.
+	pause
+	exit
+)
+for %%f in ("%x%\*.exe") do (
+    set "exe=%%~nxf"
+    goto :gotbin
+)
+echo No binary found in directory.
+pause
+exit /b
+
+:gotbin
+
+(
+    echo [SteamClient]
+    echo Exe=%exe%
+    echo ExeRunDir=.
+    echo ExeCommandLine=
+    echo AppId=6860
+    echo.
+    echo SteamClientDll=steamclient.dll
+    echo SteamClient64Dll=steamclient64.dll
+) > "%x%\ColdClientLoader.ini"
+
+xcopy "%~dp0crackfiles\*" "%x%" /E /H /I
+
+(
+    echo Cracked with Crackaholic
+    echo Do not run "%exe%", run "steamclient_loader.exe" or it will refuse to run.
+) > "%x%\README.txt"
+
+echo Done.
+pause
